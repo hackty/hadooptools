11f00377efdfb479c9360af61c51fc9e96200f3c
taoyang <hackty@163.com>
2015-11-07 13:37:13 +0800

    initial model and structure of directory

---
+++ src/main/java/com/teradata/ec/component/hadooptools/model/FileModel.java
@@ -0,0 +1,99 @@
+package com.teradata.ec.component.hadooptools.model;
+
+/**
+ * Created by taoyang on 11/5/15.
+ */
+public class FileModel {
+
+    private String id;
+
+    private String type;
+
+    private String name;
+
+    private String author;
+
+    private String size;
+
+    private String modifyTime;
+
+    private String indexTime;
+
+    private String hdfsPath;
+
+    private String highlightContent;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(String modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    public String getIndexTime() {
+        return indexTime;
+    }
+
+    public void setIndexTime(String indexTime) {
+        this.indexTime = indexTime;
+    }
+
+    public String getHdfsPath() {
+        return hdfsPath;
+    }
+
+    public void setHdfsPath(String hdfsPath) {
+        this.hdfsPath = hdfsPath;
+    }
+
+    public String getHighlightContent() {
+        return highlightContent;
+    }
+
+    public void setHighlightContent(String highlightContent) {
+        this.highlightContent = highlightContent;
+    }
+
+
+}
---
+++ src/main/java/com/teradata/ec/component/hadooptools/model/FileTypeModel.java
@@ -0,0 +1,27 @@
+package com.teradata.ec.component.hadooptools.model;
+
+/**
+ * Created by taoyang on 11/6/15.
+ */
+public class FileTypeModel {
+
+    String typeName;
+
+    Integer typeCount;
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public Integer getTypeCount() {
+        return typeCount;
+    }
+
+    public void setTypeCount(Integer typeCount) {
+        this.typeCount = typeCount;
+    }
+}
